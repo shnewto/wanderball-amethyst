112c6c577da3fad06bf0c4cf8bf319e554aa84f2
Shea Newton <shnewto@gmail.com>
2020-09-20 16:54:53 -0600

    wanderball

--- src/wanderball.rs
+++ src/wanderball.rs
@@ -13,6 +13,37 @@ pub const WANDERABLE_WIDTH: f32 = 100.0;
 
 pub const BALL_RADIUS: f32 = 2.0;
 
+pub const DOOR_HEIGHT: f32 = 8.0;
+pub const DOOR_WIDTH: f32 = 2.0;
+
+#[derive(PartialEq, Eq)]
+pub enum Wall {
+    North,
+    South,
+    East,
+    West,
+}
+
+pub struct Door {
+    pub wall: Wall,
+    pub width: f32,
+    pub height: f32,
+}
+
+impl Door {
+    fn new(wall: Wall) -> Door {
+        Door {
+            wall,
+            width: DOOR_WIDTH,
+            height: DOOR_HEIGHT,
+        }
+    }
+}
+
+impl Component for Door {
+    type Storage = DenseVecStorage<Self>;
+}
+
 #[derive(Default)]
 pub struct Wanderball;
 
@@ -22,7 +53,10 @@ impl SimpleState for Wanderball {
 
         let sprite_sheet_handle = load_sprite_sheet(world);
 
-        initialize_ball(world, sprite_sheet_handle);
+        world.register::<Door>();
+
+        initialize_doors(world, sprite_sheet_handle.clone());
+        initialize_ball(world, sprite_sheet_handle.clone());
         initialize_camera(world);
         start_audio(world);
     }
@@ -53,6 +87,56 @@ fn initialize_ball(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) 
             radius: BALL_RADIUS,
         })
         .with(local_transform)
+        .build();
+}
+
+fn initialize_doors(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) {
+    let mut north_transform = Transform::default();
+    let mut south_transform = Transform::default();
+    let mut east_transform = Transform::default();
+    let mut west_transform = Transform::default();
+
+    let y = WANDERABLE_HEIGHT / 2.0;
+    let x: f32 = WANDERABLE_WIDTH / 2.0;
+    let z: f32 = 0.0;
+
+    north_transform
+        .rotate_2d(90.0_f32.to_radians())
+        .set_translation_xyz(x, WANDERABLE_HEIGHT - DOOR_WIDTH * 2.0, z);
+    south_transform
+        .rotate_2d(90.0_f32.to_radians())
+        .set_translation_xyz(x, DOOR_WIDTH * 0.5, z);
+    east_transform.set_translation_xyz(WANDERABLE_WIDTH - DOOR_WIDTH * 0.5, y, z);
+    west_transform.set_translation_xyz(DOOR_WIDTH * 2.0, y, z);
+
+    let sprite_render = SpriteRender::new(sprite_sheet_handle, 1);
+
+    world
+        .create_entity()
+        .with(sprite_render.clone())
+        .with(Door::new(Wall::North))
+        .with(north_transform)
+        .build();
+
+    world
+        .create_entity()
+        .with(sprite_render.clone())
+        .with(Door::new(Wall::South))
+        .with(south_transform)
+        .build();
+
+    world
+        .create_entity()
+        .with(sprite_render.clone())
+        .with(Door::new(Wall::East))
+        .with(east_transform)
+        .build();
+
+    world
+        .create_entity()
+        .with(sprite_render.clone())
+        .with(Door::new(Wall::West))
+        .with(west_transform)
         .build();
 }
 
