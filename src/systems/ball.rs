ab9523f841b45412a6186120f794ab83729cf645
Shea Newton <shnewto@gmail.com>
2020-10-05 01:04:07 -0600

    keep ball sprite totally within path w/o keeping it from getting to the next path segment

--- src/systems/ball.rs
+++ src/systems/ball.rs
@@ -8,6 +8,7 @@ use amethyst::{
 
 use crate::components::{
     ball::Ball,
+    shapes::circle::Circle,
     shapes::rectangle::point_in_rect,
     wanderdata::{Pedometer, PedometerText},
 };
@@ -21,6 +22,7 @@ impl<'s> System<'s> for BallSystem {
     type SystemData = (
         WriteStorage<'s, Transform>,
         ReadStorage<'s, Ball>,
+        ReadStorage<'s, Circle>,
         Write<'s, Pedometer>,
         ReadExpect<'s, PedometerText>,
         WriteStorage<'s, UiText>,
@@ -33,6 +35,7 @@ impl<'s> System<'s> for BallSystem {
         (
             mut transforms,
             balls,
+            circles,
             mut pedometer,
             pedometer_text,
             mut ui_text,
@@ -42,25 +45,36 @@ impl<'s> System<'s> for BallSystem {
     ) {
         let movement_x = input.axis_value("move_x");
         let movement_y = input.axis_value("move_y");
-        for (transform, _) in (&mut transforms, &balls).join() {
+        for (transform, circle, _) in (&mut transforms, &circles, &balls).join() {
             let left = |segment: &PathSegmentRecord| -> f32 {
-                segment.transform.translation().x - (segment.rectangle.width * 0.5)
+                segment.transform.translation().x - (segment.rectangle.width * 0.5) + circle.radius
             };
             let bottom = |segment: &PathSegmentRecord| -> f32 {
-                segment.transform.translation().y - (segment.rectangle.height * 0.5)
+                segment.transform.translation().y - (segment.rectangle.height * 0.5) + circle.radius
             };
             let right = |segment: &PathSegmentRecord| -> f32 {
-                segment.transform.translation().x + (segment.rectangle.width * 0.5)
+                segment.transform.translation().x + (segment.rectangle.width * 0.5) - circle.radius
             };
             let top = |segment: &PathSegmentRecord| -> f32 {
-                segment.transform.translation().y + (segment.rectangle.height * 0.5)
+                segment.transform.translation().y + (segment.rectangle.height * 0.5) - circle.radius
             };
             if let Some(mv_amount) = movement_x {
                 if mv_amount.floor() as i32 != 0 {
+                    let mut threshold = circle.radius * 2.0;
+                    if mv_amount < 0.0 {
+                        threshold = -threshold;
+                    }
                     let new_x = transform.translation().x + (mv_amount as f32);
                     for segment in &(*path_segments) {
                         if point_in_rect(
                             new_x,
+                            transform.translation().y,
+                            left(segment),
+                            bottom(segment),
+                            right(segment),
+                            top(segment),
+                        ) || point_in_rect(
+                            new_x + threshold,
                             transform.translation().y,
                             left(segment),
                             bottom(segment),
@@ -88,11 +102,22 @@ impl<'s> System<'s> for BallSystem {
 
             if let Some(mv_amount) = movement_y {
                 if mv_amount.floor() as i32 != 0 {
+                    let mut threshold = circle.radius * 2.0;
+                    if mv_amount < 0.0 {
+                        threshold = -threshold;
+                    }
                     let new_y = transform.translation().y + (mv_amount as f32);
                     for segment in &(*path_segments) {
                         if point_in_rect(
                             transform.translation().x,
                             new_y,
+                            left(segment),
+                            bottom(segment),
+                            right(segment),
+                            top(segment),
+                        ) || point_in_rect(
+                            transform.translation().x,
+                            new_y + threshold,
                             left(segment),
                             bottom(segment),
                             right(segment),
